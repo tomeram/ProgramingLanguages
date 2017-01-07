363a0d110c87c9463397da5f84a13376969a1f61
Ariel Goldberger <argold8@hotmail.com>
2017-01-07 12:36:58 +0200

    Q2 completed

--- pl17_ex4/reducer.ml
+++ pl17_ex4/reducer.ml
@@ -16,3 +16,7 @@ let possible_variables = List.map (fun x -> char_to_string (char_of_int x)) ((ra
 (*
   ADD FUNCTIONS BELOW
 *)
+let rec fv = function
+	| Variable v -> StringSet.add v (StringSet.empty)
+	| Abstraction (id, t) -> StringSet.remove id (fv t)
+	| Application (t1, t2) -> StringSet.union (fv t1) (fv t2)
